89fc0eac76edceaf0fe5f134b84673780771d823
kilothejewel <Cam0197darries@gmail.com>
2026-06-30 00:57:09 +0200

    feat: create User mongoose model with password hashing and verification methods

--- models/User.js
+++ models/User.js
@@ -31,16 +31,15 @@ const userSchema = new mongoose.Schema(
 );
 
 // Hash password before saving to the database
-userSchema.pre('save', async function (next) {
+userSchema.pre('save', async function () {
   if (!this.isModified('password')) {
-    return next();
+    return;
   }
   try {
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
-    next();
   } catch (err) {
-    next(err);
+    throw err;
   }
 });
 
